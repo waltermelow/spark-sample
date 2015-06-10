4495789a8d2df35ed4200ef7e91666ac66b993bd
Yusuke Uchibori <yusubori@gmail.com>
2015-06-11 00:14:35 +0900

    update App.java

--- src/main/java/com/yusubori/spark_sample/App.java
+++ src/main/java/com/yusubori/spark_sample/App.java
@@ -14,7 +14,7 @@ public class App {
 		port(Integer.valueOf(System.getenv("PORT")));
 		
 		// routes
-		get("/", (req,res) -> "java web application sample.");
-		get("/hello", (req, res) -> "Hello World");
+		get("/", (req,res) -> "This is java web application sample.");
+		get("/hello", (req, res) -> "Hello World!!!");
 	}
 }
